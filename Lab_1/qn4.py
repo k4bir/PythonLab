e99a54a677c4d1a22e6d892537dc0df8aeb12af1
kabir <shakhyakabir1@gmail.com>
2021-03-03 11:35:53 +0545

    find hours using minutes as input

--- Lab_1/qn4.py
+++ Lab_1/qn4.py
@@ -4,6 +4,7 @@ how many hours and minutes are displayed on the 24h digital clock?
 The program should print two numbers: the number of hours (between 0 and 23)
 and the number of minutes (between 0 and 59).
 for example, if N=150, then 150 minutes have passed since midnight
+"""
 
 
 
