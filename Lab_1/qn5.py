212773854c5f03b57a1f2c2733d77a5a04305e28
kabir <shakhyakabir1@gmail.com>
2021-03-03 20:30:38 +0545

    find the number of bench

--- Lab_1/qn5.py
+++ Lab_1/qn5.py
@@ -11,6 +11,9 @@ desks. The second group has 21 students, so they can get by with no fewer than 1
 students_a=int(input("Enter the number of student in class a:"))
 students_b=int(input("Enter the number of student in class b:"))
 students_c=int(input("Enter the number of student in class c:"))
-bench_in_a=students_a/2
-if bench_in_a%2==2:
-    print(f"{bench_in_a}")
+if (students_b+students_c+students_a)%2!=0:
+    bencha=(students_a+students_b+students_c)//2+1
+    print(f"the bench{bencha}")
+else:
+    benchb=(students_a+students_b+students_c)//2
+    print(f"the bench required is {benchb}")
