37f3c83dad2548a7d302d49c5f3ae8fef3c79462
kabir <shakhyakabir1@gmail.com>
2021-03-03 11:32:53 +0545

    find hours by input in minutes

---
+++ Lab_1/qn4.py
@@ -0,0 +1,15 @@
+"""""
+4.Given the integer N - the number of minutes that is passed since midninght -
+how many hours and minutes are displayed on the 24h digital clock?
+The program should print two numbers: the number of hours (between 0 and 23)
+and the number of minutes (between 0 and 59).
+for example, if N=150, then 150 minutes have passed since midnight
+
+
+
+N=int(input('enter the time in minutes:'))
+hours=(N//60)
+minutes=(N%60)
+print(f'the hours is {hours}')
+print(f'the minutes is {minutes}')
+print(f'Its {hours}:{minutes} now')
