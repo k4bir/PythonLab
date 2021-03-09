2d48db301eb850dc789bebeb6de09c6ae03d0f9f
kabir <shakhyakabir1@gmail.com>
2021-03-09 13:57:04 +0545

    find which is faster

---
+++ Lab_1/qn7.py
@@ -0,0 +1,31 @@
+""""You live 4 miles from university. The bus drives at 25mph but spends 2 minutes at each
+of the 10 stops on the way. How long will the bus journey take? Alternatively, you could run
+to university. You could jog the first mile at 7mph; then run the next two at 15mph; before
+jogging the last at 7mph; then run the next two at 15mph; before jogging the last at 7mph
+again. Will this be quicker or slower than the bus?"""
+
+#for bus
+bus_distance=4
+bus_speed=25
+bus_t=bus_distance/bus_speed
+bus_stop=2*10
+bus_time=(bus_t*60)+bus_stop
+print(bus_time)
+
+#for jogging
+jog_distance=2
+jog_speed=7
+jog_time=(jog_distance/jog_speed)*60
+
+#for running
+run_distance=2
+run_speed=15
+run_time=(run_distance/run_speed)*60
+
+total_time=(jog_time+run_time)
+print(total_time)
+
+if bus_time>total_time:
+    print("running is faster")
+else:
+    print("bus is faster")
