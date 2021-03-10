6dfe5f78191f9fbbff4d68eb0296a96b41847aad
kabir <shakhyakabir1@gmail.com>
2021-03-10 11:28:40 +0545

    area of circle

---
+++ Lab_1/qn8.py
@@ -0,0 +1,6 @@
+"""write a python program which accepts the radius of a circle from the user and compute the area.
+(area of circle =Pi * r^2)"""
+Pi=3.14
+r=int(input("Enter the radius : "))
+area_circle = Pi * r * r
+print(f"The area of circle is {area_circle}")
